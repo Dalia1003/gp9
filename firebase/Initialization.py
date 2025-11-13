9bbc1de7d39c5a6873e72afd27ed2602f8fed0a1
Dalia1003 <dalia.alsub@gmail.com>
2025-11-13 19:21:54 +0300

    Update Initialization.py

--- firebase/Initialization.py
+++ firebase/Initialization.py
@@ -2,14 +2,11 @@ import os
 import json
 from firebase_admin import credentials, initialize_app, firestore
 
-firebase_key = os.getenv("FIREBASE_KEY")
+# Read the secret file from /etc/secrets/
+cred_path = "/etc/secrets/serviceAccountKey.json"
+cred_json = json.load(open(cred_path))
 
-if firebase_key:
-    cred = credentials.Certificate(json.loads(firebase_key))
-else:
-    # fallback for local dev
-    cred_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "serviceAccountKey.json")
-    cred = credentials.Certificate(cred_path)
-
+cred = credentials.Certificate(cred_json)
 initialize_app(cred)
+
 db = firestore.client()
