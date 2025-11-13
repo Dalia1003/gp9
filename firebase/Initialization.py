64d0403a46fcfbeab80036767609ffe15f35cc1f
Dalia1003 <dalia.alsub@gmail.com>
2025-11-13 19:42:18 +0300

    Refactor Firebase initialization code structure

--- firebase/Initialization.py
+++ firebase/Initialization.py
@@ -1,9 +1,12 @@
 import os
-from firebase_admin import credentials, initialize_app
-from firebase_admin import firestore
+from firebase_admin import credentials, initialize_app, firestore
 
+# Path to secret file on Render
 cred_path = "/etc/secrets/serviceAccountKey.json"
+
+# Initialize Firebase
 cred = credentials.Certificate(cred_path)
 initialize_app(cred)
 
+# Firestore client
 db = firestore.client()
