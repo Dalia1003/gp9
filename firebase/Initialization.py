dc6e5eeb33fd61439369c2600096fec25b0a2c88
Dalia1003 <dalia.alsub@gmail.com>
2025-11-13 19:36:32 +0300

    Reorganize import statements and initialization code

--- firebase/Initialization.py
+++ firebase/Initialization.py
@@ -1,13 +1,9 @@
-# firebase/Initialization.py
 import os
-from firebase_admin import credentials, initialize_app, firestore
+from firebase_admin import credentials, initialize_app
+from firebase_admin import firestore
 
-# Path to the secret file in Render
 cred_path = "/etc/secrets/serviceAccountKey.json"
-
-# Initialize Firebase
 cred = credentials.Certificate(cred_path)
 initialize_app(cred)
 
-# Firestore client
 db = firestore.client()
