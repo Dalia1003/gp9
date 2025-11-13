a5b8ddaa83b847008b82d54015a08b55a3052a69
Dalia1003 <dalia.alsub@gmail.com>
2025-11-13 19:11:47 +0300

    Update Initialization.py

--- firebase/Initialization.py
+++ firebase/Initialization.py
@@ -1,13 +1,15 @@
 import os
-from firebase_admin import credentials, initialize_app
+import json
+from firebase_admin import credentials, initialize_app, firestore
 
-# Get absolute path to the root of the project
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-cred_path = os.path.join(BASE_DIR, "serviceAccountKey.json")
+firebase_key = os.getenv("FIREBASE_KEY")
 
-cred = credentials.Certificate(cred_path)
+if firebase_key:
+    cred = credentials.Certificate(json.loads(firebase_key))
+else:
+    # fallback for local dev
+    cred_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "serviceAccountKey.json")
+    cred = credentials.Certificate(cred_path)
+
 initialize_app(cred)
-
-# Then your db setup
-from firebase_admin import firestore
 db = firestore.client()
