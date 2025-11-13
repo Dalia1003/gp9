e174385db2b16554d069f38d2acd7431b535cab2
Dalia1003 <dalia.alsub@gmail.com>
2025-11-13 22:59:46 +0300

    Reformat Initialization.py for consistency

--- firebase/Initialization.py
+++ firebase/Initialization.py
@@ -1,12 +1,8 @@
 import os
 from firebase_admin import credentials, initialize_app, firestore
 
-# Path to secret file on Render
 cred_path = "/etc/secrets/serviceAccountKey.json"
-
-# Initialize Firebase
 cred = credentials.Certificate(cred_path)
 initialize_app(cred)
 
-# Firestore client
 db = firestore.client()
